e17e414834d6f218ddf597885afbba5d122f9278
lintang <lintangprayogo12@gmail.com>
2024-11-10 09:06:30 +0700

    controller

--- app/Models/Shift.php
+++ app/Models/Shift.php
@@ -7,5 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Shift extends Model
 {
 
-    protected $fillable =["company_id","clock_in_time","clock_out_time","name"];
+    protected $fillable = [
+        "company_id",
+        "clock_in_time",
+        "clock_out_time",
+        "name",
+        "isSelfClocking",
+        "early_clock_in_time",
+        "allow_clock_out_till"
+    ];
 }
