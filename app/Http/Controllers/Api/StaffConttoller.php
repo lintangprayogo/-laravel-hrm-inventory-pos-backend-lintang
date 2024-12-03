1d203abd5c47eb3327e06dce2ff14c8faedfa316
lintang <lintangprayogo12@gmail.com>
2024-12-03 12:11:08 +0700

    store staff

--- app/Http/Controllers/Api/StaffConttoller.php
+++ app/Http/Controllers/Api/StaffConttoller.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class StaffConttoller extends Controller
 {
@@ -44,5 +47,51 @@ class StaffConttoller extends Controller
             "message" => 'Staff List',
             "data" => $roles
         ]);
+    }
+
+    public function store(Request $request)
+    {
+
+        $validated = $request->validate([
+            'username' => 'required|string|max:255|unique:users',
+            'is_superadmin' => 'nullable|boolean',
+            'warehouse_id' => 'nullable|exists:warehouses,id',
+            'role_id' => 'required|exists:roles,id',
+            'user_type' => 'nullable|string',
+            'login_enabled' => 'nullable|boolean',
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+            'phone' => 'required|string|max:15',
+            'profile_image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'address' => 'nullable|string',
+            'status' => 'nullable|string',
+            'department_id' => 'required|exists:departments,id',
+            'designation_id' => 'required|exists:designations,id',
+            'shift_id' => 'required|exists:shifts,id',
+        ]);
+
+        if ($request->hasFile('profile_image')) {
+            $file = $request->file('profile_image');
+            $filename = time() . '_' . $file->getClientOriginalName();
+            $filePath = $file->storeAs('images/staff', $filename, 'public');
+            $validated['profile_image'] = $filePath;
+        }
+
+        $user = new User($validated);
+        $user->company_id = 1; // Set to single company
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        $user->roles()->sync([$request->role_id]);
+
+        $user->load(['shift', 'department', 'designation', 'roles']);
+
+        return response([
+            'message' => 'Staff member created successfully',
+            'data' => $user,
+        ], 201);
+
+        $user =  new User();
     }
 }
